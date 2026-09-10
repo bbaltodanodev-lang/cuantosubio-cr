6a282c1aefa1d0648d2f3431aef0a8b5e6725455
Bernal <bbaltodano.dev@gmail.com>
2026-09-10 07:10:53 -0600

    Add files via upload

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,8 @@
 import app from "../server/index";
 
-export default app;
+// Vercel espera una función serverless explícita. El adaptador conserva todas
+// las rutas de Express y evita que el export de la instancia se trate como
+// una configuración estática.
+export default function handler(req: Parameters<typeof app>[0], res: Parameters<typeof app>[1]) {
+  return app(req, res);
+}
